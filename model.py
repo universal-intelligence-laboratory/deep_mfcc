cf9fe05e2a82d28a20cdac7c432df977b322808c
790209714@qq.com <790209714@qq.com>
2019-11-27 21:13:33 +0800

    update sampleCNN-style model

--- model.py
+++ model.py
@@ -30,40 +30,109 @@ class MFCCNet(nn.Module):
         # print(x.shape)
         B = tuple(x.shape)[0]
         mfcc = self.get_mfcc(x)
-        # print(mfcc.shape)
         B, H = tuple(x.shape)
         x = x.view(B, 20*4, -1)
-        # print(x.shape)
         x = self.c1(x)
-        # print(x.shape)
         x = self.c2(x)
-        # print(x.shape)
-        # x = self.c3(x)
-        # print(x.shape)
-        # x = x.view(B, 1, 64, -1)
-        # print(x.shape)
-        # x = self.conv1(x)
-        # print(x.shape)
-        # x = self.conv2(x)
-        # print(x.shape)
         x = x.view(B, -1)
-        # print(x.shape)
         x = self.d1(x)
         x = x.view(B, -1, 128, 81)
-        # print(x.shape)
 
         x = self.conv3(x)
-        # print(x.shape)
         x = x.view(B, 128, -1)
-        # x = F.instance_norm(x)
-        # mfcc =  F.instance_norm(mfcc)
         loss = F.mse_loss(x, mfcc)
         return loss, x, mfcc
 
 
+class MelNet(nn.Module):
+    def __init__(self):
+        super(MelNet, self).__init__()
+        self.mfcc = MelSpectrogram(sample_rate=8000)
+
+        self.c1 = nn.Conv1d(1, 128, kernel_size=400,stride=200,padding=200)
+        self.conv1 = nn.Conv2d(1, 1, kernel_size=37,padding=18)
+
+    def get_mfcc(self, x):
+        B, H = tuple(x.shape)
+        x = x.view(B, H)
+        mfcc = self.mfcc(x)
+        return mfcc
+
+    def forward(self, x):
+        B = tuple(x.shape)[0]
+        mfcc = self.get_mfcc(x)
+        # print(mfcc.shape)
+        B, H = tuple(x.shape)
+        x = x.view(B, 1, -1)
+        # print(x.shape)
+        x = self.c1(x)
+        # print(x.shape)
+        x = x.view(B,128, -1)
+        
+        loss = F.smooth_l1_loss(x, mfcc)
+        return loss, x, mfcc
+
+class SampleBlock(nn.Module):
+    def __init__(self, in_unit=128, out_unit=128, stride=1,padding=1):
+        super(SampleBlock, self).__init__()
+        self.stride = stride
+        self.c1 = nn.Conv1d(in_unit, out_unit, kernel_size=3,stride = stride,padding=padding)
+        self.bn = nn.BatchNorm1d(out_unit)
+        self.act = nn.ReLU()
+        if self.stride == 1:
+            self.max = nn.MaxPool1d(3,stride=3)
+        
+    def forward(self, x):
+        # print(x.shape)
+        x = self.c1(x)
+        x = self.bn(x)
+        x = self.act(x)
+        if self.stride == 1:
+            x = self.max(x)
+        # print(x.shape)
+        return x
+        
+
+class SampleMel(nn.Module):
+    def __init__(self):
+        super(SampleMel, self).__init__()
+        self.mfcc = MelSpectrogram(sample_rate=8000)
+
+        self.sb1 = SampleBlock(in_unit=1,stride=3)
+        self.sb2 = SampleBlock()
+        self.sb3 = SampleBlock()
+        self.sb4 = SampleBlock()
+        self.sb5 = SampleBlock()
+        self.sb6 = SampleBlock()
+        self.sb7 = SampleBlock()
+
+
+    def get_mfcc(self, x):
+        B, H = tuple(x.shape)
+        x = x.view(B, H)
+        mfcc = self.mfcc(x)
+        return mfcc
+
+    def forward(self, x):
+        B = tuple(x.shape)[0]
+        mfcc = self.get_mfcc(x)
+        # print(mfcc.shape)
+        B, H = tuple(x.shape)
+        x = x.view(B, 1, -1)
+        x = self.sb1(x)
+        x = self.sb2(x)
+        x = self.sb3(x)
+        x = self.sb4(x)
+        x = self.sb5(x)
+        # x = self.sb6(x)
+        # x = self.sb7(x)
+        x = x.view(B,128, -1)
+        mfcc = mfcc[:,:,:65]
+        loss = F.mse_loss(x, mfcc)
+        return loss, x, mfcc
+
 
 class YDataset(Dataset):
-    """普通numpy dataset"""
 
     def __init__(self, y=None, frame_len=2 * 8000):
         self.size = len(y)
--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import tqdm
 import random
 from tensorboardX import SummaryWriter
 
-from model import YDataset, MFCCNet
+from model import YDataset, SampleMel, MelNet
 from download import maybe_download_and_extract_dataset
 
 writer = SummaryWriter()
@@ -34,24 +34,28 @@ def train():
         y_all = np.load(data_path)
 
     print(y_all.shape)
-    train_loader = DataLoader(YDataset(y_all), batch_size=32, shuffle=True)
+    train_loader = DataLoader(YDataset(y_all), batch_size=256, shuffle=True)
 
-    model = MFCCNet().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    model = SampleMel().to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=0.0001)
     model.train()
 
-    for i in range(int(1e10)):
-        for batch_idx, y in enumerate(train_loader):
-            optimizer.zero_grad()
-            loss, x, mfcc = model(y.to(device))
-            pseudo_mfcc = np.expand_dims(x.detach().cpu().numpy()[0], 0)
-            real_mfcc = np.expand_dims(mfcc.detach().cpu().numpy()[0], 0)
-            writer.add_image('pseudo_mfcc', pseudo_mfcc, batch_idx)
-            writer.add_image('real_mfcc', real_mfcc, batch_idx)
-            writer.add_scalar('loss', loss.detach().cpu().numpy(), batch_idx)
-            loss.backward()
-            optimizer.step()
-            # print(loss.detach())
+    epoch = 1000
+    with tqdm.tqdm(total=epoch*(len(train_loader)//256)) as pbar:
+        for i in range(epoch):
+            for batch_idx, y in enumerate(train_loader):
+                global_step = i * epoch + batch_idx
+                optimizer.zero_grad()
+                loss, x, mfcc = model(y.to(device))
+                pseudo_mfcc = np.expand_dims(x.detach().cpu().numpy()[0], 0)
+                real_mfcc = np.expand_dims(mfcc.detach().cpu().numpy()[0], 0)
+                writer.add_image('pseudo_mfcc', pseudo_mfcc, global_step)
+                writer.add_image('real_mfcc', real_mfcc, global_step)
+                writer.add_scalar('loss', loss.detach().cpu().numpy(), global_step)
+                loss.backward()
+                optimizer.step()
+                pbar.update(1)
+                # print(loss.detach())
 
 
 if __name__ == "__main__":
