23858afef3f871795136cec2c49a22210dcb0f37
雲夢 <h.zy@mail.scut.edu.cn>
2019-12-31 23:55:04 +0800

    Update visualization

--- train.py
+++ train.py
@@ -10,10 +10,10 @@ import tqdm
 import random
 from tensorboardX import SummaryWriter
 
-from model import YDataset, SampleMel, MelNet
+from model import YDataset, SampleMel, MelnnAudio
 from download import maybe_download_and_extract_dataset
 
-writer = SummaryWriter()
+writer = SummaryWriter(log_dir='./MelnnAudio_xmask_NormNone_trainable/')
 
 def train():
     data_url = 'https://storage.googleapis.com/download.tensorflow.org/data/speech_commands_v0.02.tar.gz'
@@ -36,26 +36,30 @@ def train():
     print(y_all.shape)
     train_loader = DataLoader(YDataset(y_all), batch_size=256, shuffle=True)
 
-    model = MelNet().to(device)
+    model = MelnnAudio().to(device)
     optimizer = optim.AdamW(model.parameters(), lr=0.0001)
     model.train()
 
     epoch = 1000
-    with tqdm.tqdm(total=epoch*(len(train_loader)//256)) as pbar:
-        for i in range(epoch):
+    for i in range(epoch):
+        with tqdm.tqdm(total=len(train_loader)) as pbar:
             for batch_idx, y in enumerate(train_loader):
                 global_step = i * epoch + batch_idx
                 optimizer.zero_grad()
                 loss, x, mfcc = model(y.to(device))
-                pseudo_mfcc = np.expand_dims(x.detach().cpu().numpy()[0], 0)
-                real_mfcc = np.expand_dims(mfcc.detach().cpu().numpy()[0], 0)
-                writer.add_image('pseudo_mfcc', pseudo_mfcc, global_step)
-                writer.add_image('real_mfcc', real_mfcc, global_step)
-                writer.add_scalar('loss', loss.detach().cpu().numpy(), global_step)
                 loss.backward()
                 optimizer.step()
                 pbar.update(1)
-                # print(loss.detach())
+                
+                if batch_idx % 100 == 0:
+                    pseudo_mfcc = np.expand_dims(x.detach().cpu().numpy()[0], 0)
+                    real_mfcc = np.expand_dims(mfcc.detach().cpu().numpy()[0], 0)
+                    # mask = model.mask
+                    # writer.add_histogram('mask', mask, global_step)
+                    writer.add_image('pseudo_mfcc', pseudo_mfcc, global_step)
+                    writer.add_image('real_mfcc', real_mfcc, global_step)
+                    writer.add_scalar('loss', loss.detach().cpu().numpy(), global_step)
+                    
 
 
 if __name__ == "__main__":
